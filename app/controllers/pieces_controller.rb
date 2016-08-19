ff5304aa58c1731ca774fe96eab19db869fed1ce
Jack Pipitone <pipitone.jack@gmail.com>
2016-08-19 19:08:53 +0000

    Made it so you can't move yourself into check

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -8,10 +8,18 @@ class PiecesController < ApplicationController
 
   def update
     @piece = Piece.find(params[:id])
+    old_x = @piece.current_position_x
+    old_y = @piece.current_position_y
     @game = @piece.game
     x = params[:piece][:current_position_x].to_i
     y = params[:piece][:current_position_y].to_i
     if @piece.valid_move?(x, y)
+      @piece.update_attributes(current_position_y: x, current_position_y: y)
+      if @game.check?(@piece.color)
+        @piece.update_attributes(current_position_y: old_x, current_position_y: old_y)
+        return render text: 'Invalid move!', status: :unauthorized
+      end
+      @piece.update_attributes(current_position_y: old_x, current_position_y: old_y)
       @piece.move_to!(x, y)
       render json: nil, status: :ok
       #redirect_to game_path(@game)
