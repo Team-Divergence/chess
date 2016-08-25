0921c942cbdf41c82f00e3c37a1572c47b9ed55f
Janet Mohler <janetmohler82@gmail.com>
2016-08-25 06:37:47 +0000

    added capture logic for pawn

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,12 +1,11 @@
 class Pawn < Piece
   def valid_move?(move_to_x, move_to_y)
-    return false unless super && !obstructed?(move_to_x, move_to_y)
-    return false if current_position_x != move_to_x 
+    return false unless super
+    return false if current_position_x != move_to_x unless capture?(move_to_x, move_to_y)
     if has_moved
       if color == 'white' && (current_position_y - move_to_y) == 1
         true
-      elsif          
-        color == 'black' && (move_to_y - current_position_y) == 1
+      elsif color == 'black' && (move_to_y - current_position_y) == 1
         true
       else
         false
@@ -14,12 +13,23 @@ class Pawn < Piece
     else
       if color == 'white' && (1..2) === (current_position_y - move_to_y)
         true
-      elsif
-        color == 'black' && (1..2) === (move_to_y - current_position_y) 
+      elsif color == 'black' && (1..2) === (move_to_y - current_position_y) 
         true
       else
         false      
       end
     end      
   end
+
+  def capture?(move_to_x, move_to_y)
+    capture_piece = game.pieces.find_by(current_position_x: move_to_x, current_position_y: move_to_y)
+    if capture_piece && capture_piece.color != color
+      if (current_position_x - move_to_x) == 1 || (current_position_x - move_to_x) == -1
+        true
+      else
+        false
+      end
+    end
+  end
 end
+
